d722eaeded015b87942904c2a94bd6acd58f1c00
mahesh babu uppala <tullurivamsi833@gmail.com>
2024-12-16 18:39:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/GeeeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/GeeeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
